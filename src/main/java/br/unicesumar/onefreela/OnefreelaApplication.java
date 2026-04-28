d0dd4a36dc99bb03e036f4986d57f9e174ccfaab
eder <ederbastos.dev@gmail.com>
2026-04-28 17:00:51 -0300

    create initial project structure for springboot

---
+++ src/main/java/br/unicesumar/onefreela/OnefreelaApplication.java
@@ -0,0 +1,13 @@
+package br.unicesumar.onefreela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnefreelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnefreelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unicesumar/onefreela/OnefreelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicesumar.onefreela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnefreelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
