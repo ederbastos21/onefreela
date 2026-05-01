0a71725f3c56de6bb8ada8125e2526346af2d77c
eder <ederbastos.dev@gmail.com>
2026-05-01 17:14:59 -0300

    add method to search duplicated emails on db

--- src/main/java/br/unicesumar/onefreela/repository/UserRepository.java
+++ src/main/java/br/unicesumar/onefreela/repository/UserRepository.java
@@ -4,4 +4,5 @@ import br.unicesumar.onefreela.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail (String email);
 }
