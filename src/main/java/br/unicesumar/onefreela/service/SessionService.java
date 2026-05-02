ff2333d10cb4ce1d1b0e58d44238b3df62e53029
eder <ederbastos.dev@gmail.com>
2026-05-02 17:48:53 -0300

    [feat] Create SessionService
    1. Create SessionService that communicates with redis to create a random token and the user id in order to control session

---
+++ src/main/java/br/unicesumar/onefreela/service/SessionService.java
@@ -0,0 +1,26 @@
+package br.unicesumar.onefreela.service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class SessionService {
+    private final RedisTemplate<String,String> redisTemplate;
+
+     private SessionService (RedisTemplate<String, String> redisTemplate){
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Boolean storeSession (Long id){
+         try{
+             String token = UUID.randomUUID().toString();
+             String userId = id.toString();
+             redisTemplate.opsForValue().set(token,userId,1, TimeUnit.MINUTES);
+             return true;
+         } catch (Exception e){
+             System.out.println("erro: nao pode criar sessao no redis" + e);
+             return false;
+         }
+    }
+}
