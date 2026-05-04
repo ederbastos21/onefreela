cef1b9636f1640740daa55375ca89074114e1ecb
eder <ederbastos.dev@gmail.com>
2026-05-04 14:25:29 -0300

    [feat] create UserMapper class to convert UserDTO to User

---
+++ src/main/java/br/unicesumar/onefreela/service/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package br.unicesumar.onefreela.service.mapper;
+
+import br.unicesumar.onefreela.dto.UserRegisterDTO;
+import br.unicesumar.onefreela.dto.UserUpdateDTO;
+import br.unicesumar.onefreela.entity.User;
+
+public class UserMapper {
+    public User toUser(UserRegisterDTO dto) {
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setCpf(dto.getCpf());
+        user.setBirthday(dto.getBirthday());
+        user.setPhoneNumber(dto.getPhoneNumber());
+
+        return user;
+    }
+
+    public User toUser(UserUpdateDTO dto) {
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setBirthday(dto.getBirthday());
+        user.setPhoneNumber(dto.getPhoneNumber());
+        user.setProfilePicturePath(dto.getProfilePicturePath());
+        return user;
+    }
+}
