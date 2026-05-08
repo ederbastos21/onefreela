fc32ad44550ed327bf4099d5fc5b9b5de57c4ed1
eder <ederbastos.dev@gmail.com>
2026-05-08 15:03:29 -0300

    fix: (Spring Security Config) remove spring security path authentication

--- src/main/java/br/unicesumar/onefreela/config/SecurityConfig.java
+++ src/main/java/br/unicesumar/onefreela/config/SecurityConfig.java
@@ -5,31 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 @Profile("dev")
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
-
-        http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/admin/**").hasRole("ADMIN");
-            auth.anyRequest().authenticated();
-        });
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 
