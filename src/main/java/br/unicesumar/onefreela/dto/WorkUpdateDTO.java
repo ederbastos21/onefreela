108ea9f033b363262b902710be2c1e81534e1bd2
amauribsjr <amauribsjunior@proton.me>
2026-05-14 13:24:01 -0300

    feat(work): add work register and update DTO

---
+++ src/main/java/br/unicesumar/onefreela/dto/WorkUpdateDTO.java
@@ -0,0 +1,54 @@
+package br.unicesumar.onefreela.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class WorkUpdateDTO {
+
+    @NotBlank(message = "titulo não pode ser vazio")
+    private String title;
+
+    @NotBlank(message = "descrição não pode ser vazia")
+    private String description;
+
+    @NotBlank(message = "categoria não pode ser vazia")
+    private String category;
+
+    @NotNull(message = "preço não pode ser vazio")
+    private BigDecimal price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public WorkUpdateDTO() {}
+}
