8fd5a022f6ebd87bb68ed1695ffeeb2d0bf010d0
eder <ederbastos.dev@gmail.com>
2026-05-21 15:58:28 -0300

    feat (cart): Controller handles user authentication to avoid circular dependencies

--- src/main/java/br/unicesumar/onefreela/controller/CartController.java
+++ src/main/java/br/unicesumar/onefreela/controller/CartController.java
@@ -1,9 +1,31 @@
 package br.unicesumar.onefreela.controller;
 
+import br.unicesumar.onefreela.dto.AddCartItemDTO;
+import br.unicesumar.onefreela.entity.User;
+import br.unicesumar.onefreela.service.AuthService;
+import br.unicesumar.onefreela.service.CartService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
+
+    private final CartService cartService;
+    private final AuthService authService;
+    public CartController(CartService cartService, AuthService authService){
+        this.cartService = cartService;
+        this.authService = authService;
+    }
+
+    @PostMapping("/addItem")
+    public ResponseEntity<?> addItem (HttpServletRequest httpServletRequest, @RequestBody AddCartItemDTO addCartItemDTO){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        cartService.addItem(addCartItemDTO, user);
+        return ResponseEntity.ok().body("deu boa o carrinho");
+    }
 }
