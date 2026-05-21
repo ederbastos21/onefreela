60b4fceb3c8216ebc43193f5f58278c32a7763cf
eder <ederbastos.dev@gmail.com>
2026-05-21 01:50:16 -0300

    fix (repository): remove unused method on cartItemRepository

--- src/main/java/br/unicesumar/onefreela/repository/CartItemRepository.java
+++ src/main/java/br/unicesumar/onefreela/repository/CartItemRepository.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long>{
-    List<CartItem> findAllByUserId(Long id);
-}
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{}
