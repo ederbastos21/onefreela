55f85a7ce77b833aad46f586711f74c39214b104
eder <ederbastos.dev@gmail.com>
2026-05-24 16:59:15 -0300

    fix(cart): solve json loop bug

--- src/main/java/br/unicesumar/onefreela/controller/CartController.java
+++ src/main/java/br/unicesumar/onefreela/controller/CartController.java
@@ -1,15 +1,14 @@
 package br.unicesumar.onefreela.controller;
 
 import br.unicesumar.onefreela.dto.AddCartItemDTO;
+import br.unicesumar.onefreela.entity.Cart;
 import br.unicesumar.onefreela.entity.User;
 import br.unicesumar.onefreela.service.AuthService;
 import br.unicesumar.onefreela.service.CartService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cart")
@@ -22,10 +21,25 @@ public class CartController {
         this.authService = authService;
     }
 
+    @GetMapping("/show")
+    public ResponseEntity<?> showCartItems (HttpServletRequest httpServletRequest){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        Cart cart = user.getCart();
+        return ResponseEntity.ok().body(cart);
+    }
+
     @PostMapping("/addItem")
     public ResponseEntity<?> addItem (HttpServletRequest httpServletRequest, @RequestBody AddCartItemDTO addCartItemDTO){
         User user = authService.getAuthenticatedUser(httpServletRequest);
         cartService.addItem(addCartItemDTO, user);
-        return ResponseEntity.ok().body("deu boa o carrinho");
+        return ResponseEntity.ok().body(user.getCart().getCartItemList());
+    }
+
+    @PostMapping("/removeItem/{id}")
+    public ResponseEntity<?> removeItem (HttpServletRequest httpServletRequest, @PathVariable Long id){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+
+        cartService.removeItem(id, user);
+        return ResponseEntity.ok().body(user.getCart().getCartItemList());
     }
 }
