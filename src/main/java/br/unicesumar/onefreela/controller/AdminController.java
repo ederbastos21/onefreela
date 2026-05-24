510fac21086361bfa27b9dd3e9b05fa47ff16374
eder <ederbastos.dev@gmail.com>
2026-05-24 16:57:30 -0300

    feat(admin): create admin methods to check works and carts

--- src/main/java/br/unicesumar/onefreela/controller/AdminController.java
+++ src/main/java/br/unicesumar/onefreela/controller/AdminController.java
@@ -1,8 +1,12 @@
 package br.unicesumar.onefreela.controller;
 
+import br.unicesumar.onefreela.entity.Cart;
 import br.unicesumar.onefreela.entity.User;
+import br.unicesumar.onefreela.entity.Work;
 import br.unicesumar.onefreela.service.AuthService;
+import br.unicesumar.onefreela.service.CartService;
 import br.unicesumar.onefreela.service.UserService;
+import br.unicesumar.onefreela.service.WorkService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,17 +21,42 @@ public class AdminController {
 
     private final AuthService authService;
     private final UserService userService;
+    private final WorkService workService;
+    private final CartService cartService;
 
-    public AdminController(AuthService authService, UserService userService) {
+    public AdminController(AuthService authService, UserService userService, WorkService workService, CartService cartService) {
         this.authService = authService;
         this.userService = userService;
+        this.workService = workService;
+        this.cartService = cartService;
     }
 
     @GetMapping("/users")
-    public ResponseEntity<?> adminHome (HttpServletRequest httpServletRequest){
-        if (authService.checkAdmin(httpServletRequest)){
-            List<User> userList = userService.findAll();
-            return ResponseEntity.ok().body(userList);
+    public ResponseEntity<?> showUsers (HttpServletRequest httpServletRequest){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        if (authService.checkAdmin(httpServletRequest, user)){
+            List<User> users = userService.findAll();
+            return ResponseEntity.ok().body(users);
+        }
+        return null;
+    }
+
+    @GetMapping("/works")
+    public ResponseEntity<?> showWorks (HttpServletRequest httpServletRequest){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        if(authService.checkAdmin(httpServletRequest, user)){
+            List<Work> works = workService.findAll();
+            return ResponseEntity.ok().body(works);
+        }
+        return null;
+    }
+
+    @GetMapping("/carts")
+    public ResponseEntity<?> showCarts (HttpServletRequest httpServletRequest){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        if (authService.checkAdmin(httpServletRequest, user)){
+            List<Cart> carts = cartService.findAllCarts();
+            return ResponseEntity.ok().body(carts);
         }
         return null;
     }
