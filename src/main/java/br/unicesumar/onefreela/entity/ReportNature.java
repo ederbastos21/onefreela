77e379624a6c25c62ac418cd7bc87819014dcea3
amauribsjr <amauribsjunior@proton.me>
2026-05-25 17:40:31 -0300

    feat(report): add enums and error codes

---
+++ src/main/java/br/unicesumar/onefreela/entity/ReportNature.java
@@ -0,0 +1,11 @@
+package br.unicesumar.onefreela.entity;
+
+public enum ReportNature {
+    USER_BEHAVIOR,
+    FRAUD,
+    INAPPROPRIATE_CONTENT,
+    PAYMENT,
+    SERVICE,
+    SECURITY,
+    OTHER
+}
