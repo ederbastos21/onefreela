a70a6d8f541c1e74fb6129a821b7c41d33b31ccf
amauribsjr <amauribsjunior@proton.me>
2026-05-25 17:52:05 -0300

    feat(report): add repositories

---
+++ src/main/java/br/unicesumar/onefreela/repository/ReportRepository.java
@@ -0,0 +1,11 @@
+package br.unicesumar.onefreela.repository;
+
+import br.unicesumar.onefreela.entity.Report;
+import br.unicesumar.onefreela.entity.ReportStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findByReporterId(Long reporterId);
+    List<Report> findByStatus(ReportStatus status);
+}
