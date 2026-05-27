d9e4ff73cda2ad35655313c8f2d586c3c79eb991
eder <ederbastos.dev@gmail.com>
2026-05-26 21:40:10 -0300

    feat(repository): create repository for order

---
+++ src/main/java/br/unicesumar/onefreela/repository/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package br.unicesumar.onefreela.repository;
+
+import br.unicesumar.onefreela.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<Double, OrderItem> {
+}
