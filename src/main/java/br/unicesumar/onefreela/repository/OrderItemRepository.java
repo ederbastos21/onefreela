9372e807d6df0d21e8a65984fca8d387866fd2e5
eder <ederbastos.dev@gmail.com>
2026-05-26 21:45:48 -0300

    fix(repository): fix order of elements inside <> on order and orderItem repository

--- src/main/java/br/unicesumar/onefreela/repository/OrderItemRepository.java
+++ src/main/java/br/unicesumar/onefreela/repository/OrderItemRepository.java
@@ -3,5 +3,5 @@ package br.unicesumar.onefreela.repository;
 import br.unicesumar.onefreela.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<Double, OrderItem> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
--- src/main/java/br/unicesumar/onefreela/repository/OrderRepository.java
+++ src/main/java/br/unicesumar/onefreela/repository/OrderRepository.java
@@ -1,7 +1,8 @@
 package br.unicesumar.onefreela.repository;
 
+import br.unicesumar.onefreela.entity.Order;
 import br.unicesumar.onefreela.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Long, OrderItem> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
