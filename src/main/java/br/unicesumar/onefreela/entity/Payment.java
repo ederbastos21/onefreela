e576149064c8f30cc4c70eaea3aa310062849626
eder <ederbastos.dev@gmail.com>
2026-05-27 16:23:09 -0300

    feat(payment): add payment getter and setter

--- src/main/java/br/unicesumar/onefreela/entity/Payment.java
+++ src/main/java/br/unicesumar/onefreela/entity/Payment.java
@@ -23,4 +23,76 @@ public class Payment {
     LocalDate releasedAt;
     Double platformFee;
     Double freelancerValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public LocalDate getPaidAt() {
+        return paidAt;
+    }
+
+    public void setPaidAt(LocalDate paidAt) {
+        this.paidAt = paidAt;
+    }
+
+    public LocalDate getReleasedAt() {
+        return releasedAt;
+    }
+
+    public void setReleasedAt(LocalDate releasedAt) {
+        this.releasedAt = releasedAt;
+    }
+
+    public Double getPlatformFee() {
+        return platformFee;
+    }
+
+    public void setPlatformFee(Double platformFee) {
+        this.platformFee = platformFee;
+    }
+
+    public Double getFreelancerValue() {
+        return freelancerValue;
+    }
+
+    public void setFreelancerValue(Double freelancerValue) {
+        this.freelancerValue = freelancerValue;
+    }
 }
