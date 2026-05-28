69f559d1eb76c00d922dbfc77948b19ebac7f9f3
eder <ederbastos.dev@gmail.com>
2026-05-27 22:50:18 -0300

    feat(payment): create payment logic

---
+++ src/main/java/br/unicesumar/onefreela/dto/CardPaymentMethodDTO.java
@@ -0,0 +1,60 @@
+package br.unicesumar.onefreela.dto;
+
+import java.time.LocalDate;
+
+public class CardPaymentMethodDTO {
+    String cardNumber;
+    String name;
+    LocalDate expirationDate;
+    String cvv;
+    String cpf;
+    Long orderId;
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+}
