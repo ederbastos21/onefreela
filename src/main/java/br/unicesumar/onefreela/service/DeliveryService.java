22949c0c2976cc0ec48b4a2803eae02395a9e6a2
eder <ederbastos.dev@gmail.com>
2026-06-11 17:21:35 -0300

    feat(delivery): add methods to return orderItems based on their delivery status

--- src/main/java/br/unicesumar/onefreela/service/DeliveryService.java
+++ src/main/java/br/unicesumar/onefreela/service/DeliveryService.java
@@ -2,9 +2,13 @@ package br.unicesumar.onefreela.service;
 
 import br.unicesumar.onefreela.entity.Delivery;
 import br.unicesumar.onefreela.entity.OrderItem;
+import br.unicesumar.onefreela.entity.User;
+import br.unicesumar.onefreela.enums.OrderItemStatus;
 import br.unicesumar.onefreela.repository.DeliveryRepository;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DeliveryService {
@@ -17,5 +21,21 @@ public class DeliveryService {
 
     public Delivery save (Delivery delivery){
         return deliveryRepository.save(delivery);
+    }
+
+    public List<OrderItem> findPendingDeliveries(User user){
+        return deliveryRepository.findByFreelancerAndStatus(user, OrderItemStatus.PENDING_DELIVERY);
+    }
+
+    public List<OrderItem> findOnDispute(User user){
+        return deliveryRepository.findByFreelancerAndStatus(user, OrderItemStatus.ON_DISPUTE);
+    }
+
+    public List<OrderItem> findPendingAdjustments(User user){
+        return deliveryRepository.findByFreelancerAndStatus(user, OrderItemStatus.ADJUSTMENT_REQUEST);
+    }
+
+    public List<OrderItem> findCompleted(User user){
+        return deliveryRepository.findByFreelancerAndStatus(user, OrderItemStatus.COMPLETED);
     }
 }
