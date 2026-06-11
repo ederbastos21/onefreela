611e0ac479e653318de3b1ce7852114fa810aabe
eder <ederbastos.dev@gmail.com>
2026-06-11 16:53:56 -0300

    feat(delivery): create dinamic delivery query

--- src/main/java/br/unicesumar/onefreela/repository/DeliveryRepository.java
+++ src/main/java/br/unicesumar/onefreela/repository/DeliveryRepository.java
@@ -1,9 +1,22 @@
 package br.unicesumar.onefreela.repository;
 
 import br.unicesumar.onefreela.entity.Delivery;
+import br.unicesumar.onefreela.entity.OrderItem;
+import br.unicesumar.onefreela.entity.User;
+import br.unicesumar.onefreela.enums.OrderItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+    @Query("""
+    SELECT oi FROM OrderItem oi
+    WHERE oi.work.user = :freelancer
+    AND oi.status = :status
+    """)
+
+    List<OrderItem> findByFreelancerAndStatus(User freelancer, OrderItemStatus status);
 }
