adad8c1a8b6156853a1830005ca58c9c5b2eb13e
eder <ederbastos.dev@gmail.com>
2026-06-11 18:14:00 -0300

    feat(Delivery): Create delivery endpoints for freelancers to show their jobs

--- src/main/java/br/unicesumar/onefreela/controller/DeliveryController.java
+++ src/main/java/br/unicesumar/onefreela/controller/DeliveryController.java
@@ -3,41 +3,63 @@ package br.unicesumar.onefreela.controller;
 import br.unicesumar.onefreela.dto.DeliverDTO;
 import br.unicesumar.onefreela.entity.OrderItem;
 import br.unicesumar.onefreela.entity.User;
+import br.unicesumar.onefreela.entity.Work;
 import br.unicesumar.onefreela.service.AuthService;
 import br.unicesumar.onefreela.service.DeliveryService;
 import br.unicesumar.onefreela.service.OrderService;
+import br.unicesumar.onefreela.service.WorkService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/delivery")
 public class DeliveryController {
 
     private final AuthService authService;
     private final DeliveryService deliveryService;
+    private final OrderService orderService;
 
-    public DeliveryController (AuthService authService, DeliveryService deliveryService){
+    public DeliveryController (AuthService authService, DeliveryService deliveryService, OrderService orderService){
         this.authService = authService;
         this.deliveryService = deliveryService;
+        this.orderService = orderService;
     }
 
-    @GetMapping("/findDeliveries")
-    public ResponseEntity<?> findDeliveries (HttpServletRequest httpServletRequest){
+    @GetMapping("/findPending")
+    public ResponseEntity<?> findPending (HttpServletRequest httpServletRequest){
         User user = authService.getAuthenticatedUser(httpServletRequest);
+        return ResponseEntity.ok().body(deliveryService.findPendingDeliveries(user));
     }
 
-    @GetMapping("/findPendingDeliveries")
-    public ResponseEntity<?> findPendingDeliveries (HttpServletRequest httpServletRequest){
+    @GetMapping("/findOnDispute")
+    public ResponseEntity<?> findOnDispute (HttpServletRequest httpServletRequest){
         User user = authService.getAuthenticatedUser(httpServletRequest);
+        return ResponseEntity.ok().body(deliveryService.findOnDispute(user));
+    }
+
+    @GetMapping("/findPendingAdjustments")
+    public ResponseEntity<?> findPendingAdjustment (HttpServletRequest httpServletRequest){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        return ResponseEntity.ok().body(deliveryService.findPendingAdjustments(user));
+    }
+
+    @GetMapping("/findCompleted")
+    public ResponseEntity<?> findCompleted (HttpServletRequest httpServletRequest){
+        User user = authService.getAuthenticatedUser(httpServletRequest);
+        return ResponseEntity.ok().body(deliveryService.findCompleted(user));
     }
 
     @PostMapping("/makeDelivery")
     public ResponseEntity<?> makeDelivery (HttpServletRequest httpServletRequest, @ModelAttribute DeliverDTO deliverDto){
         User user = authService.getAuthenticatedUser(httpServletRequest);
+        return ResponseEntity.ok().body(orderService.makeDelivery(user, deliverDto));
     }
+    
 }
