c07a2b9324dec3de0b9df6d1a21812c379e89781
eder <ederbastos.dev@gmail.com>
2026-06-11 16:54:48 -0300

    feat (deliveryDTO): add getter/setter and add List of files

--- src/main/java/br/unicesumar/onefreela/dto/DeliverDTO.java
+++ src/main/java/br/unicesumar/onefreela/dto/DeliverDTO.java
@@ -2,8 +2,34 @@ package br.unicesumar.onefreela.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public class DeliverDTO {
     String message;
-    MultipartFile file;
-    Long workId;
+    List<MultipartFile> files;
+    Long orderItemId;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(Long orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public List<MultipartFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<MultipartFile> files) {
+        this.files = files;
+    }
 }
